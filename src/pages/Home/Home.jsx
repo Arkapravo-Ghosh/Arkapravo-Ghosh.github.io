72ca7e5e8cc6f22158c62326684d17a432063b98
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-05-07 06:32:19 +0530

    Minor Fixes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,26 +12,28 @@ const typestrings = [
 export default function Home() {
   return (
     <div className="Home">
-      <div className="Home-Header">
-        <h1>Hi, I'm Arkapravo Ghosh</h1>
-      </div>
-      <div className="Home-Typewriter">
-        <Typewriter
-          options={{
-            strings: typestrings,
-            autoStart: true,
-            loop: true,
-            deleteSpeed: 20,
-          }}
-        />
-      </div>
-      <div className="Home-Buttons">
-        <Link to="/about" style={{ textDecoration: 'none' }}>
-          <button className="Home-Button"><p>About Me</p></button>
-        </Link>
-        <Link to="/contact" style={{ textDecoration: 'none' }}>
-          <button className="Home-Button"><p>Contact</p></button>
-        </Link>
+      <div className="Home-Main">
+        <div className="Home-Header">
+          <h1>Hi, I'm Arkapravo Ghosh</h1>
+        </div>
+        <div className="Home-Typewriter">
+          <Typewriter
+            options={{
+              strings: typestrings,
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 20,
+            }}
+          />
+        </div>
+        <div className="Home-Buttons">
+          <Link to="/about" style={{ textDecoration: 'none' }}>
+            <button className="Home-Button"><p>About Me</p></button>
+          </Link>
+          <Link to="/contact" style={{ textDecoration: 'none' }}>
+            <button className="Home-Button"><p>Contact</p></button>
+          </Link>
+        </div>
       </div>
       <div className="Home-Side">
         <img src={Side} alt="Side Image" />

1741407357420246349304ee76806b6087b625b7
Arkapravo-Ghosh <ghosharkapravo1612005@gmail.com>
2023-05-08 12:14:11 +0530

    Added Animated Cursor and other Minor Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-} from "react-router-dom";
-import './styles/App.css'
-import NavBar from './components/NavBar/NavBar'
-import Footer from './components/Footer/Footer'
+} from 'react-router-dom';
+import './styles/App.css';
+import AnimatedCursor from 'react-animated-cursor';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Portfolio from './pages/Portfolio/Portfolio';
@@ -27,6 +28,15 @@ const router = createBrowserRouter(
 export default function App({ routes }) {
   return (
     <div className="App">
+      <AnimatedCursor
+        color='58, 163, 216'
+        outerAlpha={0.3}
+        innerSize={13.5}
+        outerSize={13.5}
+        outerScale={6}
+        innerScale={0.8}
+        trailingSpeed={8}
+      />
       <React.Fragment>
         <RouterProvider router={router} />
         <Footer />
